91bb82d1d7479d12d17b802945f8b5a882d3c79e
YangDou613 <d860613@gmail.com>
2024-02-06 17:02:32 +0800

    update Assignment-2

--- Week-4/Assignment-2/app.js
+++ Week-4/Assignment-2/app.js
@@ -12,28 +12,22 @@ function ajax(src, callback) {
 function render(data) {
 	for (var i = 0; i < data.length; i++) {
 
-		// create new div
 		let div = document.createElement(`div`);
 		div.id = `product${i}`
 
-		// name
 		let name = document.createElement('h1');
 		name.textContent = data[i].name;
 
-		// price
 		let price = document.createElement('p');
 		price.textContent = `$ ${data[i].price}`;
 
-		// description
 		let description = document.createElement('p');
 		description.textContent = data[i].description;
 
-		// add to fragment
 		div.appendChild(name);
 		div.appendChild(price);
 		div.appendChild(description);
 
-		// inner to THML
 		document.body.appendChild(div);
 	}
 }
